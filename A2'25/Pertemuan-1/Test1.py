ac0073389805e42eeb90bf3192bf90507d9cb9b9
Nurul Latifa <nulaatifa@gmail.com>
2025-09-23 15:10:45 +0800

    initial commit

---
+++ A2'25/Pertemuan-1/Test1.py
@@ -0,0 +1,19 @@
+
+# print ("""Halo,
+# Saya sedang belajar ngoding
+# Bahasa Pemrograman python""")
+
+# a = 10
+# b = 3
+# c = a + b
+# print(c)
+
+# a = 0,13
+# b = 0,15
+# c = a+b
+# print(c)
+
+# Nama = 'Sifwah'
+# Hobi = "ngoding"
+# print(Nama)
+# print(Hobi)
