1f24cf94a53692276f184b520cbd7d19a1953f7c
Nurul Latifa <nulaatifa@gmail.com>
2025-09-25 21:52:01 +0800

    Tambah folder Pertemuan 3 berisi Pertemuan-3.py

---
+++ A2'25/Pertemuan 3/Pertemuan-3.py
@@ -0,0 +1,126 @@
+
+#angka=5
+
+#if angka >5:
+# print("angka lebih besar 5")
+
+#cuaca="hujan" 
+
+#if cuaca=="hujan":
+# print("dirumah aja")
+#else :
+ #print("Nongkrong")
+
+# nilai=70
+
+# if nilai>=70:
+#  print ("lulus")
+# else:
+#  print("tidak lulus")
+
+#contoh 3
+
+# cuaca='mendung'
+
+# if cuaca=="hujan":
+#     print("Dirumah aja")
+# elif cuaca == "mendung":
+#     print("Makan mie")
+# else :
+#     print("Nongkrong")
+
+
+#contoh 4
+
+# usia=int(input("Masukan usia"))
+
+# if usia<=0:
+#     print("usia tidak valid")
+# elif usia <=13:
+#     print("anak-anak")
+# elif usia<=18:
+#     print("Remeja")
+# elif usia<=40:
+#     print("Dewasa")
+# else:
+#     print("Orang Tua")
+
+#contoh 5
+
+# nilai=int(input("Masukan Nilai:"))
+
+# if nilai>=90:
+#     print("Nilai A")
+# elif nilai>=70:
+#     print("Nilai B")
+# elif nilai>=60:
+#     print("Nilai C")
+# else :
+#     print("Nilai D")
+
+# nilai= 70
+# nilai=int(input("nilai:"))
+
+# status="lulus" if nilai>=70 else "Tidak lulus"
+# print(status)
+
+##Nested if
+
+# a= 2
+# b= 5
+# c= 6
+
+# if a<b :
+#     if a<c:
+#         print("A paling kecil")
+#     else:
+#         print("C lebih kecil dari a")
+# elif a<c:
+#     print("C paling besar")
+# else:
+#     print("A paling besar")
+
+
+
+
+
+#Studi Kasus 1
+
+# Tinggi_Badan=float(input("Masukan Tinggi Badan(cm)"))
+
+# status="diizinkan naik wahan" if Tinggi_Badan>=145 else "tidak diizinkan naik wahana"
+# print(status)
+
+#Studi Kasus 2
+
+# Total_Belanja=int(input("Masukan Total Belanja :   "))
+
+# if Total_Belanja >100000:
+#     diskon = Total_Belanja*.02
+#     Bayar= Total_Belanja-diskon
+#     print("selamat anda mendapatkan diskon 20%,dan membayar :",{Bayar})
+
+# elif Total_Belanja >50000:
+#     diskon=Total_Belanja*0.1
+#     Bayar=Total_Belanja-diskon
+#     print("Selamat anda mendapatkan diskon 10%,Anda membayar :",{Bayar})
+
+# else:
+#     Bayar=Total_Belanja
+#     print("Maaf anda tidak mendapatkan diskon :",{Bayar})
+
+
+# nilai=int(input("Masukan nilai:  "))
+# Kelas=input("Masukan Kelas:   ")
+
+# if nilai >=80 and (Kelas=="A" or Kelas=="a"):
+#     print("IPK 4")
+
+# elif nilai>=80 and Kelas=="B":
+#     print("IPK 3")
+
+# else :
+#     print("IPK 2")
+
+
+
