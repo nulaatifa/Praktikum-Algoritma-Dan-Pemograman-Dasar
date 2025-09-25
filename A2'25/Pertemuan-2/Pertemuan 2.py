c398d34d0456f4fd57de6735e822927e4e9ffb4d
Nurul Latifa <nulaatifa@gmail.com>
2025-09-25 22:03:21 +0800

    Tambah folder Pertemuan-2 berisi Pertemuan 2.py

---
+++ A2'25/Pertemuan-2/Pertemuan 2.py
@@ -0,0 +1,54 @@
+
+#Nama = "Daffa"
+#nama = "Nabil"
+#Angka = '123'
+
+#print (Nama)
+#print (type (Angka))
+
+#print ("""Halo,
+#Saya sedang belajar ngoding
+#Bahasa Pemrograman python""")
+
+#print ("halo""saya""Daffa")
+#print("halo"+ '  '+"daffa")
+
+
+#teks = 'saya suka belajar apd'
+#print(teks[0:5])
+#print(teks[:5])
+
+#Hujan = False
+#Panas = True
+
+#hasil = Hujan and Panas
+#print(hasil)
+#prodi = ['Informatika', True, 1,  'TeknikSipil']
+
+#tuple = ('Lian' , 19, True , 'APD')
+
+
+#operator1 = not (1>5)
+#print (operator1)
+
+
+# studi kasus
+#nama_siswa = 'Daffa'
+#nilai_1 = 85.5
+#nilai_2 = 91.0
+#nilai_3 = 77.5
+#nilai_4 = 88.9
+
+#nilai_siswa= [nilai_1,nilai_2,nilai_3,nilai_4]
+#print(nilai_siswa)
+#total = sum(nilai_siswa)
+#print(total)
+
+#harga_barang=int(input('masukan harga barang'))
+#ajak=(harga barang*0.11)
+
+#harga_akhir=(harga_barang*pajak)
+
+#print(pajak yang harus dibayar:',pajak')
+
+
