e7d2849866c5a9fb1685ea4f256615ac035db3b9
Nurul <nulaatifa@gmail.com>
2025-10-10 22:13:34 +0800

    Menambahkan file di Pertemuan 5

---
+++ A225/Pertemuan-5/Pertemuan5.2.py
@@ -0,0 +1,108 @@
+
+#Membuat list
+# Matakuliah = ["PTI", "Apd", "Kalkulus", "Orsikom"]
+
+# for index,i in enumerate(Matakuliah):
+#     print(f"Mata Kuliah {index} : {i} ")
+
+# Matakuliah1 = []
+
+# Matakuliah =[ ]
+
+# print(dir(list))
+
+#Membaca list
+
+# print(Matakuliah[1:3])
+
+# print(Matakuliah[-1])
+
+# print(Matakuliah[1:3:2])
+
+# Matakuliah.append = ("Matematika")
+# print(Matakuliah)
+
+# Matakuliah.insert(2, "Matematika")
+# Matakuliah.insert(0, "Jarkom")
+# print(Matakuliah)
+
+# menampilkan list
+# praktikum = ["Mahasiswa", 20, True, 45.10, ["APD",25]]
+# # memanggil satu elemen
+# print(praktikum[1])
+# # memanggil elemen di dalam list
+# print(praktikum[4][1])
+
+# list awal
+# studyclub = ["Data Science", "Robotics", "Multimedia", "Network"]
+# Tambahkan Web
+# studyclub.append("Web")
+# print(studyclub)
+
+# del Matakuliah [2]
+# print(Matakuliah)
+# Matakuliah.remove('Kalkulus')
+# print(Matakuliah)
+
+# Matakuliah.pop(3)
+
+#pop bisa memberitau apa yang di hapus
+
+# hapus = Matakuliah.pop(3)
+# print(Matakuliah)
+# print(hapus)
+
+# list = [1, 2, 3]
+
+# print (len(list))
+# print (min(list))
+# print (max(list))
+# print (sum(list))
+
+# print (sum(list)/len(list))
+
+# nilai = [4, 5, 6, "dante"]
+
+# hasil = list + nilai
+# kali = list * 3
+
+# print(f"Ini hasil penjumlahan list {hasil}")
+# print(f"Ini hasil perkalian list {kali}")
+
+# Kelas = [
+#     ["Ridho", "Lian", "Nabil"],
+#     ["Daffa", "Dante", "Santoso"],
+#     ["Pernanda", "Riyadi", "Ahnaf"],
+#]
+
+# Kelas[1].insert(3, "Mutia")
+# print(Kelas[1])
+# print(Kelas[0][1])
+# print(Kelas[0][2])
+# print(Kelas[2][1])
+
+# for index,i in enumerate(Kelas):
+#     print(f"K=Kelas {index} : {i} ")
+
+# for index, i in enumerate(Kelas):
+#     for nama in i:
+#         print(nama, index)
+
+#Mendefinisikan tuple
+
+# Anggota = ("riyadi", 20, True, 3.96, ["APD",25],("samarinda",12))
+
+# print(Anggota)
+# print(Anggota[4][0])
+# print(Anggota[-1][0])
+
+# studyclub = ["Data Science", "Robotics", "Multimedia", "Network"]
+
+# liststudy =list(studyclub)
+
+# liststudy[1] = "Web"
+# tuplestudy = tuple(liststudy)
+
+# print(f"ini tuple : {studyclub}")
+# print(f"ini list : {liststudy}")
+# print(f"ini tuple : {tuplestudy}")
---
+++ A225/Pertemuan-5/Pertemuan5.py
@@ -0,0 +1,120 @@
+#Membuat List
+# Mata_Kuliah=["APD","KALKULUS","ORSIKOM"]
+# Mata_Kuliah1=[]
+
+# #Membaca List
+
+# # print(Mata_Kuliah[1:3])
+
+# print(Mata_Kuliah[0])
+
+# # print(Mata_Kuliah[1:3:2])
+
+# Mata_Kuliah.append("Matematika") 
+
+# print(Mata_Kuliah)
+
+
+
+# # list awal
+# studyclub = ["Data Science", "Robotics", "Multimedia", "Network"]
+# # Tambahkan Web
+# studyclub.append("Web")
+# print(studyclub)
+# # output
+# ['Data Science', 'Robotics', 'Multimedia', 'Network', 'Web']
+
+# # list awal
+# studyclub = ["Data Science", "Robotics", "Multimedia", "Network"]
+# # Tambahkan Web
+# studyclub.insert(2,"Web")
+# print(studyclub)
+
+# # list awal
+# studyclub = ["Data Science", "Robotics", "Multimedia", "Network"]
+# print(studyclub)
+
+# studyclub[2] = "AI"
+# print(studyclub)
+
+
+
+# matakuliah = ['PTI', 'APD','Kalkulus','Diskrit']
+# # print(matakuliah)     
+
+# # # del matakuliah[2]
+# # print(matakuliah)
+
+# matakuliah = ['PTI', 'APD','Kalkulus','Diskrit']
+
+# for index,i in enumerate(matakuliah):
+#     print(index,i)
+
+# for i in matakuliah:
+#     print(f"Print Mata Kuliah {i}")
+# print(matakuliah)
+# # print(matakuliah)
+
+#  matakuliah.remove('Kalkulus')
+# matakuliah.pop(3)
+
+# hapus=matakuliah.pop(3)
+# print(matakuliah)
+
+# list=[1,2,3]
+
+# print(sum(list)/len(list))
+
+# print()
+
+# list=[1,2,3]
+# nilai=[4,5,6]
+
+# hasil=list+nilai
+# kali=list*3
+# print(hasil)
+
+# print(f"Ini hasil penjumlahan list {hasil}")
+# print(f"ini hasil Perkalian list {kali}")
+
+# kelas = [
+#     ["Ridho", "Lian", "Nabil"],
+#     ["Daffa", "Dante", "Santoso"],
+#     ["Pernanda", "Riyadi", "Ahnaf"],
+# ]
+
+# for i in kelas:
+#     for nama in i:
+#         print(nama)
+
+# kelas[1].insert(1,"Bakil")
+# print(kelas[1])
+
+#TUPLE
+
+# anggota = ("riyadi", 20, True, 3.96, ["APD",25],("samarinda",12))
+
+# print(anggota[4][0])
+# print(anggota[5][0])
+# print(anggota[-1])
+
+
+# studyclub = ("Data Science", "Robotics", "Multimedia", "Network")
+
+# liststudy=list(studyclub)
+
+# liststudy[1]= "Web"
+
+# tuplestudy=tuple(liststudy)
+
+# print(f"ini tuple :{studyclub}")
+# print(f"ini list = {liststudy}")
+# print(f"ini tuple = {tuplestudy}")
+
+#STUDY KASUS 
+
+keranjang=["Brokoli","Apel","Jamur","Nanas","Wortel","kiwi","kol","sawi","Lobak"]
+
+for i in keranjang:
+    for keranjang in i:
+        print(keranjang)
